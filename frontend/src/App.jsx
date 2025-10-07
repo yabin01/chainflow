6beaa4f240a56e502e3f33e58b6df5191535c376
yabin01 <yabin9701@icloud.com>
2025-10-08 00:06:37 +0800

    feat: add frontend base files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>ChainFlow - 区块链信息聚合平台</h1>
+      <p>开发中...</p>
+    </div>
+  )
+}
+
+export default App
